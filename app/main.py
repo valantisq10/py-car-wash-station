d37d99ee465c80ea3ecc150536026340940f2e08
Valantis Lempidakis <balantislempidakis@gmail.com>
2024-12-08 21:42:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,22 +2,36 @@ from typing import List
 
 
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class: int = comfort_class
         self.clean_mark: int = clean_mark
         self.brand: str = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center: float = distance_from_city_center
         self.clean_power: int = clean_power
         self.average_rating: float = average_rating
         self.count_of_ratings: int = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = (car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center)
+        price = (car.comfort_class
+                 * (self.clean_power - car.clean_mark)
+                 * self.average_rating
+                 / self.distance_from_city_center
+                 )
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
@@ -35,4 +49,6 @@ class CarWashStation:
     def rate_service(self, new_rating: int) -> None:
         total_rating = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
-        self.average_rating = round((total_rating + new_rating) / self.count_of_ratings, 1)
+        self.average_rating = round(
+            (total_rating + new_rating)
+            / self.count_of_ratings, 1)
